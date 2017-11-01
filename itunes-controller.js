d69a9eff31e4ff9cfa91f2a53355122265bbe06a
Morgan Waters <morangwaters@gmail.com>
2017-11-01 14:43:25 -0600

    Intial Commit

--- itunes-controller.js
+++ itunes-controller.js
@@ -1,18 +1,19 @@
-function ItunesController(){
+function ItunesController() {
   var itunesService = new ItunesService()
   //Do Not Modify the getMusic function
-  this.getMusic = function getMusic(e){
+  this.getMusic = function getMusic(e) {
     e.preventDefault();
     var artist = e.target.artist.value;
     itunesService.getMusicByArtist(artist).then(draw); //after get music by artist returns what are you doing with the objects?
   }
 
   //Start coding here
-  function draw(){
+  function drawList() {
+    var songList = itunesService.songList;
   }
 
 
 
 
-  
+
 }
