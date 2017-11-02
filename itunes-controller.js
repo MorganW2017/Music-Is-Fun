f096a663ef7baeede51d67d4332d8f86a245364a
Morgan Waters <morangwaters@gmail.com>
2017-11-02 16:40:42 -0600

    Style, services, controllers, oh my...

--- itunes-controller.js
+++ itunes-controller.js
@@ -1,21 +1,38 @@
 function ItunesController() {
   var itunesService = new ItunesService()
+  //Do Not Modify the getMusic function
   this.getMusic = function getMusic(e) {
     e.preventDefault();
     var artist = e.target.artist.value;
-    itunesService.getMusicByArtist(artist).then(drawList);
+    itunesService.getMusicByArtist(artist).then(draw); //after get music by artist returns what are you doing with the objects?
   }
   //Start coding here
-  function drawList() {
-    var songList = itunesService[songList];
-    return songList
-  }
-}
-document.addEventListener('play', function (e) {
-  var audios = document.getElementsByClassName('audio');
-  for (var i = 0, len = audios.length; i < len; i++) {
-    if (audios[i] != e.target) {
-      audios[i].pause();
+
+  function draw(songList) {
+    console.log(songList)
+    var template = ''
+    for (var i = 0; i < songList.length; i++) {
+      var song = songList[i]
+      var albumArt = song.albumArt.replace('100x100bb', '500x500bb')
+      var templates = {
+        'song': ` 
+        <div class="col-sm-12 col-md-6 flex v-center h-center">
+        <div id="song-wrapper" class="song-wrapper panel panel-default text-center">
+        <div class="panel-body">
+        <img class="song-image" src="${albumArt}" alt="">
+        <p class="song-title">${song.title}</p>
+        <p class="artist-name">${song.artist}</p>
+        <p class="album-title">${song.collection}</p>
+        <audio src="${song.preview}" controls></audio>                    
+        <p class="album-price">Album Price: 
+        ${(song.currency === 'USD') ? ('$' + song.albumPrice) : (song.albumPrice + ' ' + song.currency)}</p>
+        </div>
+        </div>
+        </div>
+      `}
+      template += templates[song.fileType]
+      document.getElementById('songs').innerHTML = template
+      console.clear()
     }
   }
-}, true);
+}
--- itunes-service.js
+++ itunes-service.js
@@ -1,20 +1,31 @@
 function ItunesService() {
   this.getMusicByArtist = function (artist) {
+
+    //allows requests to localhost: 8080 otherwise blocked by itunes
     var url = '//bcw-getter.herokuapp.com/?url=';
     var url2 = 'https://itunes.apple.com/search?term=' + artist;
     var apiUrl = url + encodeURIComponent(url2);
-    $('#get-music-button').text('FETCHING "GOOD" MUSIC CHOICES....');
+
+    //changes the button to loading while songs load
+    $('#get-music-button').text('LOADING....');
+
+    //modifies the objects to reduce the excess data
     return $.getJSON(apiUrl).then(function (response) {
       var songList = response.results.map(function (song) {
         return {
           title: song.trackName,
-          albumArt: song.artworkUrl60,
+          albumArt: song.artworkUrl100,
           artist: song.artistName,
           collection: song.collectionName,
-          price: song.collectionPrice,
-          preview: song.previewUrl
+          albumPrice: song.collectionPrice,
+          trackPrice: song.trackPrice,
+          preview: song.previewUrl,
+          currency: song.currency,
+          fileType: song.kind,
+          genre: song.primaryGenreName
         };
       })
+      //changes button back to GET MUSIC once songs are loaded
       $('#get-music-button').text('Get Music');
       return songList;
     })
